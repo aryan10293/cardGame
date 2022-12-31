1e2ca072a08666f508e193e88349ee1c7df7a543
aondrej ryan <aondrejryan@gmail.com>
2022-12-31 01:43:55 -0800

    finished blackjack game version 1.0

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import  startGame  from "./start.js";
 let deckId = 0
 let userScore = 0
 let aiScore = 0
+let startedGame = false
 const score1 = document.getElementById('score1')
 const score2 = document.getElementById('score2')
 const player1 = document.getElementById('playerOne')
@@ -17,6 +18,7 @@ fetch(`https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6`)
 
 
 function blackJack(){
+    startedGame = true
     const url = `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
     fetch(url)
         .then(res => res.json()) // parse response as JSON
@@ -25,7 +27,19 @@ function blackJack(){
             // console.log(deckId)
             start(data)
 
-            const twoSec =  setTimeout(drawSecondcard, 2000, data,deckId)
+            const twoSec =  setTimeout(drawSecondcard, 200, data,deckId)
+            console.log(userScore)
+            if(userScore === 21){
+                alert('Gameover you hit blackjack')
+                // player1.src = ''
+                // player2.src = ''
+                userScore = 0
+                aiScore = 0
+                score1.innerText = userScore
+                score2.innerText = aiScore
+                startedGame = false
+            }
+            
         })
         .catch(err => {
             console.log(deckId)
@@ -47,7 +61,7 @@ function blackJack(){
 }
 function convertToNumUser(value){
     if(value === 'ACE'){
-        if(confirm("you've drawn an ACE!! hit conform for 11 or cancel for 1")){
+        if(confirm("you've drawn an ACE!! hit ok for 11 or cancel for 1")){
             return 11
         } else {
             return 1
@@ -89,38 +103,108 @@ function hit(){
     fetch(url)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
-            // console.log(data)
-            // console.log(deckId)
-           // alert('drawing card')
             const player1 = document.getElementById('playerOne')
             player1.src = data['cards'][0]['image']
             userScore += convertToNumUser(data['cards'][0]['value'])
             score1.innerText = userScore
 
             if(userScore > 21){
-              //  alert('Gameover you went over 21 you LOST')
-                player1.src = ''
-                player2.src = ''
+                alert('Gameover you went over 21 you LOST')
+                // player1.src = ''
+                // player2.src = ''
                 userScore = 0
                 aiScore = 0
                 score1.innerText = userScore
                 score2.innerText = aiScore
-            }
+            } else if(userScore === 21){
+                alert('Gameover you hit blackjack')
+                // player1.src = ''
+                // player2.src = ''
+                userScore = 0
+                aiScore = 0
+                score1.innerText = userScore
+                score2.innerText = aiScore
+            } 
         })
         .catch(err => {
             console.log(deckId)
             console.log(`error ${err}`)
     });
 }
+function stay(){
+    score2.innerText = aiScore
+    if(aiScore <= 16){
+        const url = `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
+        fetch(url)
+            .then(res => res.json()) // parse response as JSON
+            .then(data => {
+                // console.log(data)
+                // console.log(deckId)
+               // alert('drawing card')
+               const player2 = document.getElementById('playerTwo')
+                //player2.src = data['cards'][1]['image']
+                aiScore += convertToNumAi(data['cards'][0]['value'])
+                score2.innerText = aiScore
+                if(aiScore > 21){
+                    alert('Gameover Dealer went over 21 you WON')
+                    // player1.src = ''
+                    // player2.src = ''
+                    userScore = 0
+                    aiScore = 0
+                    score1.innerText = userScore
+                    score2.innerText = aiScore
+                } else if(aiScore === 21){
+                    alert('Gameover Dealer hit blackjack')
+                    // player1.src = ''
+                    // player2.src = ''
+                    userScore = 0
+                    aiScore = 0
+                    score1.innerText = userScore
+                    score2.innerText = aiScore
+                } else if (aiScore > userScore){
+                    alert(`Gameover Dealer scored:${aiScore} and you scored:${userScore} you LOST!!!`)
+                    // player1.src = ''
+                    // player2.src = ''
+                    userScore = 0
+                    aiScore = 0
+                    score1.innerText = userScore
+                    score2.innerText = aiScore
+                }  else if (userScore > aiScore){
+                    alert(`Gameover Dealer scored:${aiScore} and you scored:${userScore} you WON!!!`)
+                    // player1.src = ''
+                    // player2.src = ''
+                    userScore = 0
+                    aiScore = 0
+                    score1.innerText = userScore
+                    score2.innerText = aiScore
+                } else if(aiScore === userScore){
+                    alert(`user score:${userScore} dealer score:${aiScore} TIE!!!! keep playing`)
+                    // player1.src = ''
+                    // player2.src = ''
+                    userScore = 0
+                    aiScore = 0
+                    score1.innerText = userScore
+                    score2.innerText = aiScore
+                } 
+                console.log(aiScore)
+                console.log(userScore)
+            })
+            .catch(err => {
+                console.log(deckId)
+                console.log(`error ${err}`)
+        });
+    }
+}
 document.querySelector('#start').addEventListener('click', blackJack)
 document.querySelector('#hitMe').addEventListener('click', hit)
+document.querySelector('#stay').addEventListener('click', stay)
 //  // display card for player 1 and display card for ai 
 // // display another card for player 1 also display his total value and continue to show ai first card but hide second 
 // // cards 2-10 are scored as is jacks kings and queens are equal to 10
 // // aces can equal 1 or 11 give user that choice and make sure ai has correct logic to make right descion
 // // if user wants to continue to play make a hit he can draw another card
 // else he can stay and will be out of the game 
-// if player stays show ai cards or value
+// // if player stays show ai cards or value
 // if ai is 16 or under  he draws another card if 17 0or over then he stays
-// if dealer bust over 21 player 1 wins 
+// // if dealer bust over 21 player 1 wins 
 // if dealer doesnt bust then if your score is higher than the ai then you win 
